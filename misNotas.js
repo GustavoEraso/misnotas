639d7edf134d1a2f425f102fb26f6b181aae52dd
Gustavo Eraso <gustavoeraso1988@gmail.com>
2022-01-19 00:03:56 -0300

    agregue foto de perfil y modifique el header

---
+++ misNotas.js
@@ -0,0 +1,34 @@
+// var cards =[
+//     {titulo: 'GitHub', parrafo: 'aca va la info', link: 'platzi.com'},
+//     {titulo: 'GitHub', parrafo: 'aca va la info', link: 'platzi.com'},
+//     {titulo: 'GitHub', parrafo: 'aca va la info', link: 'platzi.com'},
+
+// ];
+
+// function tarjetaNueva(titulo, parrafo, link) {
+//     this.titulo = titulo;
+//     this.parrafo = parrafo;
+//     this.link = link;
+// }
+
+// // var tarjeta = "<div  class= \"card card-1\"><div class=\"caja-h3\" ><h3>" + cards[1].titulo + "</h3></div><section class=\"contendor-card-parrafo\"><p>" + 
+// //                             cards[1].parrafo +"</p></section><input type=\"button\" id=\"verDetalles1\" class=\"button-detalles\" value=\"Ver Detalles\"></div>";
+
+// var TarjetasHTML ="";                            
+
+// cards.forEach(function(tarjeta){
+
+//     TarjetasHTML = TarjetasHTML + "<div  class= \"card card-1\"><div class=\"caja-h3\" ><h3>" + tarjeta.titulo + "</h3></div><section class=\"contendor-card-parrafo\"><p>" + 
+//     tarjeta.parrafo +"</p></section><input type=\"button\" id=\"verDetalles1\" class=\"button-detalles\" value=\"Ver Detalles\"></div>";
+
+
+
+
+
+// });
+
+
+
+// var divContenedor = document.getElementById("prueba");
+
+// divContenedor.outerHTML = TarjetasHTML;
